091af6be2986631f7c9f8f5ca9789361d8a7bcd6
Fatih Karaman <fatih.karaman.dn@aygaz.com.tr>
2022-06-17 14:25:25 +0300

    Add project files.

---
+++ ClassLibraryApp/Calc.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibraryApp
+{
+    public class Calc
+    {
+        public int sum(int a, int b) => a + b;
+
+        public List<int> FindOddNumbers(int start, int end)
+        {
+            List<int> oddNumbers = new List<int>();
+
+            for (int i = start; i < end; i++)
+            {
+                if (i%2==1)
+                    oddNumbers.Add(i);
+            }
+
+            return oddNumbers;
+        }
+    }
+}
---
+++ xUnitTestApp.Test/CalcTest.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibraryApp;
+
+namespace xUnitTestApp.Test
+{
+
+    public class CalcTest
+    {
+        [Fact]
+        public void SumTest()
+        {
+            //Arrange
+            int a = 1;
+            int b = 2;
+            var calc = new Calc();
+
+            //Act
+            var result = calc.sum(a, b);
+
+            //Assert
+            Assert.Equal<int>(3, result);
+        }
+
+        [Fact]
+        public void FindOddNumbersTest()
+        {
+            //Arrange
+            int start = 1;
+            int end = 10;
+            var calc = new Calc();
+
+            //Act
+            var result = calc.FindOddNumbers(start, end);
+
+            //Assert
+            Assert.Contains(3, result);
+        }
+
+    }
+}
